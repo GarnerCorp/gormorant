9855a38cbb874e315ae6c47d2fc7eba85e8a7158
rbbowd <rob.bowd@garnercorp.com>
2025-09-15 20:58:09 -0400

    Brickie updates

--- modules/parser/src/main/scala/io/chrisdavenport/cormorant/parser/CSVLikeParser.scala
+++ modules/parser/src/main/scala/io/chrisdavenport/cormorant/parser/CSVLikeParser.scala
@@ -19,17 +19,17 @@ import cats.syntax.all._
   *
   * aaa,bbb,ccc CRLF zzz,yyy,xxx CRLF
   *
-  * 2. The last record in the file may or may not have an ending line break. For
-  * example:
+  *   2. The last record in the file may or may not have an ending line break.
+  *      For example:
   *
   * aaa,bbb,ccc CRLF zzz,yyy,xxx
   *
-  * 3. There maybe an optional header line appearing as the first line of the
-  * file with the same format as normal record lines. This header will contain
-  * names corresponding to the fields in the file and should contain the same
-  * number of fields as the records in the rest of the file (the presence or
-  * absence of the header line should be indicated via the optional "header"
-  * parameter of this MIME type). For example:
+  *   3. There maybe an optional header line appearing as the first line of the
+  *      file with the same format as normal record lines. This header will
+  *      contain names corresponding to the fields in the file and should
+  *      contain the same number of fields as the records in the rest of the
+  *      file (the presence or absence of the header line should be indicated
+  *      via the optional "header" parameter of this MIME type). For example:
   *
   * field_name,field_name,field_name CRLF aaa,bbb,ccc CRLF zzz,yyy,xxx CRLF 4.
   * Within the header and each record, there may be one or more fields,
@@ -40,21 +40,21 @@ import cats.syntax.all._
   *
   * aaa,bbb,ccc
   *
-  * 5. Each field may or may not be enclosed in double quotes (however some
-  * programs, such as Microsoft Excel, do not use double quotes at all). If
-  * fields are not enclosed with double quotes, then double quotes may not
-  * appear inside the fields. For example:
+  *   5. Each field may or may not be enclosed in double quotes (however some
+  *      programs, such as Microsoft Excel, do not use double quotes at all). If
+  *      fields are not enclosed with double quotes, then double quotes may not
+  *      appear inside the fields. For example:
   *
   * "aaa","bbb","ccc" CRLF zzz,yyy,xxx
   *
-  * 6. Fields containing line breaks (CRLF), double quotes, and commas should be
-  * enclosed in double-quotes. For example:
+  *   6. Fields containing line breaks (CRLF), double quotes, and commas should
+  *      be enclosed in double-quotes. For example:
   *
   * "aaa","b CRLF bb","ccc" CRLF zzz,yyy,xxx
   *
-  * 7. If double-quotes are used to enclose fields, then a double-quote
-  * appearing inside a field must be escaped by preceding it with another double
-  * quote. For example:
+  *   7. If double-quotes are used to enclose fields, then a double-quote
+  *      appearing inside a field must be escaped by preceding it with another
+  *      double quote. For example:
   *
   * "aaa","b""bb","ccc"
   */
